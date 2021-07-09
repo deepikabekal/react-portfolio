7ff4884bfd80deb2a8befa8c3eb718403f36bf5b
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-10 00:35:11 +0400

    Navigation section added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import About from "./Components/About";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <>
+      <Nav />
       <About />
     </>
   );
---
+++ src/Components/Nav/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <header>
+            <h1>Deepika Bekal</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#about-me">About Me</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#contact-me">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#resume">Resume</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav
