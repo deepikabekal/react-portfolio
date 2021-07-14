837976bd7c077e41750d8aff1e8d418e6e12a173
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-15 00:03:48 +0400

    footer style added

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
-            <i className="fab fa-github"></i>
-            <i className="fab fa-linkedin"></i>
+            <i className="fab fa-github fa-3x"></i>
+            <i className="fab fa-linkedin fa-3x"></i>
         </div>
     )
 }
