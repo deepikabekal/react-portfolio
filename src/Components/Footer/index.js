e1fcf7e5b3212966fe8945e8168252cd516a6d2d
Deepika Bekal <deepika.bekal2@gmail.com>
2021-07-17 18:54:46 +0400

    footer stackoverflow logo added

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -6,7 +6,10 @@ const Footer = () => {
             <a href="https://github.com/deepikabekal">
                 <i className="fab fa-github footer-icon fa-3x"></i>
             </a>
-            <a href="">
+            <a href="https://stackoverflow.com/users/15129233/deepika-bekal">
+                <i className="fab fa-stack-overflow footer-icon fa-3x"></i>
+            </a>
+            <a href="www.linkedin.com/in/deepika-bekal">
                 <i className="fab fa-linkedin footer-icon fa-3x"></i>
             </a>
             
